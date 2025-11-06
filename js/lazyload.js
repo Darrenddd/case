751bd797fbaadbead3dd779b558008713f1fb61b
Darrenddd <daiyiliyu@gmail.com>
2025-11-06 20:50:34 +0800

    lazy

--- js/lazyload.js
+++ js/lazyload.js
@@ -5,7 +5,7 @@ const options = {
     // rootMargin 的格式：'上 右 下 左'，值必須是 px 或 %。
     // '0px 0px 300px 0px' 表示將底部偵測範圍擴大 300 像素。
     // 這樣圖片在距離視口底部 300px 遠時就會觸發加載。
-    rootMargin: '0px 0px 1000px 0px',
+    rootMargin: '0px 0px 2000px 0px',
     threshold: 0 // 只要有一點點進入擴展區域就觸發
 };
 
